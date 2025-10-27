695db130ba1036873452fc89eace2a0c255a7c65
Menna Mohamed <mennamohamed199514@gmail.com>
2025-10-27 22:26:28 +0300

    fix: resolve merge conflicts

--- src/hooks/use-products-filters.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { FilteredProductsApi } from "@/lib/apis/FilteredProducts.api";
-
-export function useProductsQuery(filters: Filters) {
-  return useQuery({
-    queryKey: ["products", filters],
-    queryFn: () => FilteredProductsApi(filters),
-  });
-}
--- src/lib/apis/get-products.api.ts
+++ src/lib/apis/get-products.api.ts
@@ -1,18 +1,33 @@
 // Types
-type GetProductsParams = { page?: number; limit?: number };
+type GetFilteredProductsParams = Filters & {
+  page?: number;
+  limit?: number;
+};
 
 export async function GetProductsApi({
   page = 1,
   limit = 12,
-}: GetProductsParams) {
+  category,
+  rating,
+  occasion,
+  price,
+}: GetFilteredProductsParams) {
   // Build url
   const url = new URL(`${process.env.NEXT_PUBLIC_API}/products`);
+
+  // Add pagination
   url.searchParams.set("page", String(page));
   url.searchParams.set("limit", String(limit));
 
+  // Add filters if exist
+  if (category) url.searchParams.append("category", category);
+  if (rating) url.searchParams.append("rating", rating.toString());
+  if (occasion) url.searchParams.append("occasion", occasion);
+  if (price) url.searchParams.append("price", price);
+
   // Fetch data
   const response = await fetch(url.toString());
-  const payload: ApiResponse<PaginatedResponse<{ products: Product[] }>> =
+  const payload: ApiResponse<PaginatedResponse<ProductsResponse>> =
     await response.json();
 
   // Handle error
@@ -21,5 +36,5 @@ export async function GetProductsApi({
   }
 
   // Return result
-  return payload as SuccessResponse<PaginatedResponse<{ products: Product[] }>>;
+  return payload;
 }
