61e167fda9f76f97bc7a0eebca1478c55f52abc7
abdalla-mazen <armn404040@gmail.com>
2025-10-30 18:43:57 +0300

    fix(layout): resolved issues in navbar and footer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,3 @@
-import Footer from "@/components/layout/footer";
-import Header from "@/components/layout/header";
-
 import HeroPage from "./hero/page";
 import BestSelling from "@/components/best-seliing";
 import MostPopular from "@/components/most-popular/most-popular";
@@ -14,18 +11,14 @@ export default function Page({ params }: { params: { locale: string } }) {
   const locale = params.locale;
 
   return (
-    <>
-      <Header />
-      <div className="flex flex-col justify-center items-center mx-auto max-w-7xl">
-        <HeroPage />
-        <BestSelling locale={locale} />
-        <MostPopular locale={locale} />
-        <About />
-        <Gallery />
-        <TestimonialPage />
-        <Companies />
-      </div>
-      <Footer />
-    </>
+    <div className="flex flex-col justify-center items-center mx-auto max-w-7xl">
+      <HeroPage />
+      <BestSelling locale={locale} />
+      <MostPopular locale={locale} />
+      <About />
+      <Gallery />
+      <TestimonialPage />
+      <Companies />
+    </div>
   );
 }
