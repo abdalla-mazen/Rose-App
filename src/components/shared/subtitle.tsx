93de7de9397565a41adfc5239a7429d3bc644f9b
Seif Eldin Khaled <seifeldinn4@gmail.com>
2026-03-14 15:36:58 +0200

    fix: Some edits in homepage respoinsive

--- src/components/shared/subtitle.tsx
+++ src/components/shared/subtitle.tsx
@@ -1,8 +1,15 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export default function Subtitle({ title }: { title: string }) {
   return (
-    <h2 className="before:-bottom-0.5 after:bottom-0 after:-z-10 before:absolute after:absolute relative before:bg-softPink-600 after:bg-softPink-100 dark:before:bg-softPink-500 dark:after:bg-zinc-700 after:rounded-r-md before:w-1/3 after:w-3/4 before:h-[2px] after:h-[40%] font-bold text-maroon-600 dark:text-softPink-200 text-4xl before:content-[''] after:content-[''] before:start-0 after:start-0">
+    <h2
+      className={cn(
+        "capitalize relative font-bold mx-auto md:mx-0 text-maroon-600 dark:text-softPink-200 text-xl md:text-3xl lg:text-4xl",
+        "before:absolute before:-bottom-0.5 before:start-0 before:w-1/3 before:h-[2px] before:bg-softPink-600 dark:before:bg-softPink-500 before:content-['']",
+        "after:absolute after:bottom-0 after:start-0 after:-z-10 after:w-3/4 after:h-[40%] after:bg-softPink-100 dark:after:bg-zinc-700 after:rounded-r-md after:content-['']",
+      )}
+    >
       {title}
     </h2>
   );
